9d5153385793bcc3b9297019507154872a3fd700
Marcus Patman <marcus@adventureonthewave.com>
2026-08-30 00:24:26 -0600

    fix: Python 3 compatibility — filter() is not subscriptable
    list(filter(...))[0] instead of filter(...)[0], per the documented known
    issue in README.md.

--- Backup_running_instances.py
+++ Backup_running_instances.py
@@ -7,7 +7,7 @@ instances = ec2.instances.filter(
     Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
 
 for instance in instances:
-    instance_name = filter(lambda tag: tag['Key'] == 'Name', instance.tags)[0]['Value']
+    instance_name = list(filter(lambda tag: tag['Key'] == 'Name', instance.tags))[0]['Value']
 
     for volume in ec2.volumes.filter(Filters=[{'Name': 'attachment.instance-id', 'Values': [instance.id]}]):
         description = 'scheduled_snapshot-%s.%s' % (instance_name, volume.volume_id)
